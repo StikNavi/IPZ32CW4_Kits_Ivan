8596f7b58e30151d66b57db757c8de0afb078d20
StikNavi <stiknavi@gmail.com>
2024-03-14 14:32:00 +0200

    Commit

--- app/src/main/java/ua/edu/lntu/cw4/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw4/MainActivity.kt
@@ -3,13 +3,15 @@ package ua.edu.lntu.cw4
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ua.edu.lntu.cw4.ui.theme.IPZ32CW4_Kits_IvanTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +19,26 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             IPZ32CW4_Kits_IvanTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                TaskListScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun TaskListScreen() {
+    val tasks = remember { mutableStateListOf("Task 1", "Task 2", "Task 3") }
+
+    Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(text = "Список завдань")
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            LazyColumn {
+                items(tasks) { task ->
+                    TaskListItem(task = task)
+                    Divider()
                 }
             }
         }
@@ -30,17 +46,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TaskListItem(task: String) {
+    Surface(modifier = Modifier.fillMaxWidth(), color = MaterialTheme.colorScheme.surface) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(text = task)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun TaskListPreview() {
     IPZ32CW4_Kits_IvanTheme {
-        Greeting("Android")
+        TaskListScreen()
     }
 }
